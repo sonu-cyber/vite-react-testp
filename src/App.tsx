890cb3a55974346b8c4f2d39d989df0b7a53b78a
Gayathri raghuram <gayathriraghuram1@gmail.com>
2026-06-01 14:34:03 +0530

    SOme modifications to files after Netlify did nor build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Home from "./pages/Home";
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import styles from "./about.module.css";
 import styled from "styled-components";
