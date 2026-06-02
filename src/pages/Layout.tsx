ade05da79817ce049c26c9486178b3857c11026d
Gayathri raghuram <gayathriraghuram1@gmail.com>
2026-06-02 06:46:28 +0530

    Modified Layout.tsx

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styles from "../pages/layout.module.css";
+import styles from "./layout.module.css";
 
 export default function Layout() {
   return (
